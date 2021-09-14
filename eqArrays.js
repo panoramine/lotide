f114591b8a87631277cc1e94be0998a280657477
panoramine <camila.c.a.salles@gmail.com>
2021-09-14 15:47:15 -0700

    function that checks if two arrays are equals and return a boolean

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayA, arrayB) {
+  let areEquals = true;
+  if (arrayA.length !== arrayB.length) {
+    areEquals = false;
+  } else {
+    for (let i = 0; i < arrayB.length; i++) {
+      if (arrayB[i] !== arrayA[i]) {
+        areEquals = false;
+      }
+    }
+  }
+  return areEquals;
+};
+
