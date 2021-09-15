186b9d325e413ecb57582347431399ac8bfdc3a5
panoramine <camila.c.a.salles@gmail.com>
2021-09-15 13:48:11 -0700

    function that receives an array and an object and return a count of elements in the array determined by the given object

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countOnly = function(allItems, itemsToCount) {
+  let results = {};
+  for (let key in itemsToCount) {
+    if (itemsToCount[key] === true) {
+      let counter = 0;
+      for (let i = 0; i < allItems.length; i++) {
+        if (allItems[i] === key) {
+          counter += 1;
+        }
+      }
+      if (counter > 0) {
+        results[key] = counter;
+      }
+      
+    }
+  }
+  return results;
+}
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
