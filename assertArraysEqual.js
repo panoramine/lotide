8854ce85d28e9b30963e2b584671d8bd0a16a299
panoramine <camila.c.a.salles@gmail.com>
2021-09-20 13:35:14 -0700

    refactored assertArraysEqual to use module.exports and require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(arrayA, arrayB) {
-  let areEquals = true;
-  if (arrayA.length !== arrayB.length) {
-    areEquals = false;
-  } else {
-    for (let i = 0; i < arrayB.length; i++) {
-      if (arrayB[i] !== arrayA[i]) {
-        areEquals = false;
-      }
-    }
-  }
-  return areEquals;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -19,3 +7,6 @@ const assertArraysEqual = function(array1, array2) {
     console.log("❌❌❌ Assertion Failed: the arrays are not equal!");
   }
 };
+
+
+module.exports = assertArraysEqual;
